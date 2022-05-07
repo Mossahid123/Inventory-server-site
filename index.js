4eadbb82c6b41864d54a08613d172a024f7718d7
Mossahid123 <mossahidhossain309@gmail.comm>
2022-05-08 00:06:56 +0600

    server connected

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/' , (req , res ) =>{
+    res.send('server is connected')
+})
+
+app.listen(port , ()=>{
+    console.log('listening to the port' , port)
+})
