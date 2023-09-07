11e6c404937ede7d871c4b28d1c1b7f9c8beeec9
Mossahid123 <mossahidhossain309@gmail.comm>
2023-09-07 22:18:57 +0600

    data checking

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +13,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fo0cp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
         await client.connect();
@@ -80,7 +80,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server is connected')
+    res.send('server is running')
 })
 
 app.listen(port, () => {
